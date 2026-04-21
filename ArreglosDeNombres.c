d8003ab1cbd0959209f8533f3ceb12974424ff19
WhiteDLN <white14dl@gmail.com>
2026-04-21 04:09:03 -0300

    Agregar funcion indice

--- ArreglosDeNombres.c
+++ ArreglosDeNombres.c
@@ -9,9 +9,15 @@ void MostrarPersonas(char *Vector[]) {
     }
 }
 
+void BuscarNombre(int Seleccion, char *Vector[]){
+    Seleccion = Seleccion - 1;
+    printf("\nEl nombre seleccionado es:\n");
+    printf("%s\n", Vector[Seleccion]);
+}
+
 int main() {
     char *Vector[5]; 
-    
+    int Seleccion = 0;
     
     for (int i = 0; i < 5; i++) {
         Vector[i] = (char*)malloc(sizeof(char) * 20); // Asignar memoria para cada nombre, respetando el maximo de 20
@@ -19,6 +25,16 @@ int main() {
         fgets(Vector[i], 20, stdin);
 
        
+    }
+
+    printf("Ingrese un número =<5 para ver el nombre almacenado en el.\n");
+    scanf("%d", &Seleccion);
+    if (Seleccion <= 5 && Seleccion >= 1) {
+        BuscarNombre(Seleccion, Vector);
+    }
+
+    else{
+        printf ("No se ha encontrado el valor buscado.\n");
     }
 
     MostrarPersonas(Vector); // Mostrar los nombres ingresados
